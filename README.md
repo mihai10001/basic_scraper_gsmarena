3676a8ffec2097c33625f0c98511e6a12dadc6cb
Iliuta Mihai <mortvmis@gmail.com>
2018-10-07 04:59:10 +0300

    v1.0 does all the work by itself

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# scrap
+# Unobstructive scraper for building a database
+
+### Small scraper for building a mobile device database
+### It is used togheter with a web application personal project, for a phone guide website (specifically, for displaying entries in the device database).
+### I intend by no means to steal or distribute any data I gathered/ will gather. It is intended for the web application project only, which is kept private (the site won't get published).
+
+**There are 3 major steps in the implementation, each assigned to a function:**
+
+- The first function reads all known brands from a list of brands.
+- For every brand, it returns a list of the latest 40 devices (by release date) from that brand (checks only first page of results).
+- For each devices, it only scrapes a slight portion of information:
+ - Release date
+ - Brand
+ - Name
+ - Processor
+ - Rating
+ - Price
+
+After scraping the information for one device, it stores it into the database, and sleeps for 0.1 seconds (in case I am sending too many requests to the server).
+In the implementation, I have used libraries such as urllib to open URLs, and the well known Python library BeautifulSoup for parsing the HTML.
+If you've got this far with the reading, feel free to reuse my code in your project, or message me if you find that there could be important improvements considered.
--- scrap_and_update.py
+++ scrap_and_update.py
@@ -1,42 +1,74 @@
+import time
 import urllib.request
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 
-def scrape_info():
+def scrape_brands():
     results = []
-    pages = []
+    brands_page = 'https://www.gsmarena.com/makers.php3'
+    webpage = urllib.request.urlopen(brands_page)
+    soup = BeautifulSoup(webpage, 'html.parser')
 
-    for page in pages:
-        item = {}
-        webpage = urllib.request.urlopen(page)
-        soup = BeautifulSoup(webpage, 'html.parser')
-        name_box = soup.find('h1', attrs={'class': 'specs-phone-name-title'})
-        date_box = soup.find('td', attrs={'class': 'nfo', 'data-spec': 'year'})
-        proc_box = soup.find('div', attrs={'data-spec': 'chipset-hl'})
-        price_box = soup.find('td', attrs={'class': 'nfo', 'data-spec': 'price'})
-
-        name = name_box.text.strip()
-        date = date_box.text.strip()
-
-        if price_box:
-            price = price_box.text.strip()
-            item["price"] = price.split()[1] + ' $'
-        else:
-            item["price"] = '-'
-
-        item["rating"] = '-'
-        item["model"] = name
-        item["brand"] = name.split()[0]
-        item["date"] = ' '.join(date.split(', '))
-        item["proc"] = proc_box.text.strip()
-        results.append(item)
+    for tr in soup.find_all('tr'):
+        for td in tr.findChildren('td'):
+            results.append('https://www.gsmarena.com/' + td.find('a')['href'])
 
     return results
 
 
-client = MongoClient('localhost', 27017)
-db = client['local']
-collection = db['phone']
-res = scrape_info()
-collection.insert_many(res)
+# only scrapes the first page of results = maximum 40 phones from each brand
+# sorted by release date
+def scrape_devices_from_brand(brand_page):
+    results = []
+
+    webpage = urllib.request.urlopen(brand_page)
+    soup = BeautifulSoup(webpage, 'html.parser')
+    phones_box = soup.find('div', attrs={'class': 'makers'})
+    ul = phones_box.find('ul')
+
+    for li in ul.find_all('li'):
+        results.append('https://www.gsmarena.com/' + li.find('a')['href'])
+
+    return results
+
+
+# ugly one liners for simplified view
+def scrape_device_info(device_page):
+    item = {}
+    webpage = urllib.request.urlopen(device_page)
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    name_box = soup.find('h1', attrs={'class': 'specs-phone-name-title'})
+    date_box = soup.find('td', attrs={'class': 'nfo', 'data-spec': 'year'})
+    proc_box = soup.find('div', attrs={'data-spec': 'chipset-hl'})
+    price_box = soup.find('td', attrs={'class': 'nfo', 'data-spec': 'price'})
+    rating_box = soup.find('strong', attrs={'class': 'accent'})
+
+    item["price"] = price_box.text.strip().split()[1] + ' $' if price_box else '-'
+    item["rating"] = rating_box.text.strip() if rating_box else '-'
+    item["date"] = ' '.join(date_box.text.strip().split(', ')) if date_box else '-'
+    item["proc"] = proc_box.text.strip() if proc_box else '-'
+
+    if name_box:
+        item["model"] = name_box.text.strip()
+        item["brand"] = name_box.text.strip().split()[0] 
+    else:
+        item["model"], item["brand"] = '-', '-'
+
+    return item
+
+
+def scrape_and_update_db():
+    client = MongoClient('localhost', 27017)
+    db = client['local']
+    collection = db['phone']
+
+    for brand in scrape_brands():
+        for device in scrape_devices_from_brand(brand):
+            print("Inserting device and sleeping 0.1 secs.")
+            collection.insert_one(scrape_device_info(device))
+            time.sleep(0.1)
+
+
+scrape_and_update_db()
